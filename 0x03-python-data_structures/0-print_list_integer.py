90708a6f01833ea7e1cff15bff0cf767a8e90ad8
henrypheonix2 <henryogazi@yahoo.com>
2023-08-13 15:58:45 +0000

    commit

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def print_list_integer(my_list=[]):
+<<<<<<< HEAD
+        for i in my_list:
+                    print("{:d}".format(i))
+=======
+    for i in my_list:
+        print("{:d}".format(i))
+>>>>>>> 0aeafb600e3885943a1e0a103ba359d2323d71b6
